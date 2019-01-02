1624d9ecdcaad9b915ab10b9ed1518cf9675a094
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-02 19:01:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,26 +18,26 @@ class Scraper
     students
   end
 
-  def self.scrape_profile_page(profile_url)
-    html = File.read(profile_url)
-    profile_page = Nokogiri::HTML(html)
-    student_details = {}
-    social_networks = profile_page.css("div.social-icon-container a").collect{|network| network.attribute("href").value}
-    social_networks.each do |link|
-      if link.include?("twitter")
-        student_details[:twitter] = link
-      elsif link.include?("linkedin")
-        student_details[:linkedin] = link
-      elsif link.include?("github")
-        student_details[:github] = link
-      else
-        student_details[:blog] = link
-      end
-    end
-    student_details[:profile_quote] = profile_page.css("div.profile-quote").text
-    student_details[:bio] = profile_page.css("div.description-holder p").text
-    student_details
-  end
+#  def self.scrape_profile_page(profile_url)
+#    html = File.read(profile_url)
+#    profile_page = Nokogiri::HTML(html)
+#    student_details = {}
+#    social_networks = profile_page.css("div.social-icon-container a").collect{|network| network.attribute("href").value}
+#    social_networks.each do |link|
+#      if link.include?("twitter")
+#        student_details[:twitter] = link
+#      elsif link.include?("linkedin")
+#        student_details[:linkedin] = link
+#      elsif link.include?("github")
+#        student_details[:github] = link
+#      else
+#        student_details[:blog] = link
+#      end
+#    end
+#    student_details[:profile_quote] = profile_page.css("div.profile-quote").text
+#    student_details[:bio] = profile_page.css("div.description-holder p").text
+#    student_details
+#  end
 
 
   def self.scrape_districts(url_uk_postcode)
